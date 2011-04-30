2be61101b723baa31a584fa590e756ec4a45fa9b
John Athayde <john.athayde@livingsocial.com>
2011-04-30 01:21:47 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sunnystate::Application.config.secret_token = '2e3c82a5a437fed9459460f3350c660b7ff244932417ce718e51ae20d5a9570c04f4c519eac1b84a91951922dd9d02779009d916f783ea9e72cd5842ae4bc027'
